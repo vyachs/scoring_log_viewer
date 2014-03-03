cfb7d8467ed35ac19f3e0a85b283014c13287a7b
vyachs <Vyacheslav.Soldatkin@kodep.ru>
2014-03-03 18:02:11 +0400

    beg

--- models/audit_event.rb
+++ models/audit_event.rb
@@ -1,32 +1,33 @@
 class AuditEvent < ActiveRecord::Base
+
+  def self.analyze_log_files
+    #%w(frontend_audit support_audit db_audit app_audit).each do |file_name|
+    %w(frontend-server-audit.log).each do |file_name|
+      analyze_log_file(file_name)
+    end
+  end
+
   def self.analyze_log_file(file_name)
-    hh = 0
-    gg = 0
-    File.open('analyzed_logs/frontend-server-audit.log') do |f|
+    File.open("analyzed_logs/#{file_name}_diff.log") do |f|
       str = f.gets
       while str do
         if str =~ /CRED_DISP/ && str =~ /sshd/
-          hh += 1
+          new(server_name: file_name, kind: 'logout').save_params(str)
         elsif str =~ /USER_ACCT/ && str =~ /sshd/
-          gg += 1
+          new(server_name: file_name, kind: 'login').save_params(str)
         end
         str = f.gets
       end
     end
-    [hh, gg]
   end
 
-  def self.creation(event_kind, str)
-    time_moment = str[0..14]
-
+  def save_params(str)
+    ip = str.match(/addr=(?<ip>.*?) /)[:ip]
+    if ip != '?'
+      self.user_name = str.match(/acct="(?<user_name>.*?)"/)[:user_name]
+      self.ip = ip
+      self.time_moment = str[0..14]
+      save
+    end
   end
 end
-
-#frontend_audit_diff.log
-#
-#support_audit_diff.log
-#
-#db_audit_diff.log
-#
-#app_audit_diff.log
-#
